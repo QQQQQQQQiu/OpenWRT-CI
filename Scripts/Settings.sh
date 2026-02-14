ced69dbd23e18d6a307053d6f2f6dc30cd9d2224
Qiu <841371467@qq.com>
2026-02-14 14:57:55 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -51,12 +51,12 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	echo "CONFIG_FEED_nss_packages=n" >> ./.config
 	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
 	#开启sqm-nss插件
-	echo "CONFIG_PACKAGE_luci-app-sqm=n" >> ./.config
-	echo "CONFIG_PACKAGE_sqm-scripts-nss=n" >> ./.config
+	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
+	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 	#设置NSS版本
 	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
 	#if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=n" >> ./.config
+		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	#else
 	#	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
 	#fi
